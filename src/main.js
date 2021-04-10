7f6fc05f7ce55e9191805187b4ddfa1fc83570b7
jacksong“ <984288606@qq.com>
2021-04-10 23:31:33 +0800

    封装axios

--- src/main.js
+++ src/main.js
@@ -6,12 +6,21 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 import axios from 'axios'
+// axios全局配置
+axios.defaults.baseURL='http://123.207.32.32:8000'
+axios.defaults.timeout=430
+// axios处理并发网络请求
 axios.all([axios({
-            url: 'http://123.207.32.32:8000/home/multidata'
+            // baseURL: 'http://123.207.32.32:8000',
+            // timeout: 5000,
+            url: '/home/multidata'
         }),
         axios({
-            url: 'http://123.207.32.32:8000/home/data?type=sell&page=1',
-            param: {
+            // baseURL: 'http://123.207.32.32:8000',
+            // timeout: 5000,
+            url: '/home/data?type=sell&page=1',
+            // get请求的参数是params，post对应（request body）data：{key:'aa'}
+            params: {
                 type: 'sell',
                 page: 1
             }
