c37b618370897f4875fcc87aaa7ab132cad1d4a3
李山高“ <984288606>
2021-04-10 23:01:08 +0800

    访问coderwhy的接口

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import axios from 'axios'
+axios.all([axios({
+            url: 'http://123.207.32.32:8000/home/multidata'
+        }),
+        axios({
+            url: 'http://123.207.32.32:8000/home/data?type=sell&page=1',
+            param: {
+                type: 'sell',
+                page: 1
+            }
+        })
+    ])
+    // .then(results => console.log(results))
+    .then(axios.spread((result1, result2) => console.log(result2)))
